01b2939944d9073210ac9cfcc503887029854e1d
Miguel Parrilla <miquelpl@telefonica.net>
2017-12-14 13:28:07 +0100

    Add files via upload

---
+++ LOESUNGEN-14122017.sql
@@ -0,0 +1,64 @@
+/*
+Lesen Sie im Buch
+
+Kapitel 10:
+Seite 204 bis 208	DML-Trigger
+
+1. Verhindern Sie den Verkauf, wenn die Ware nicht auf Lager ist
+2. Nach dem Verkauf Meldung anzeigen, wenn weniger als 5 Artikel vorhanden sind und 
+   einen Bestellhinweis anzeigen, wenn der Bestand geringer als 2 ist
+3. Erzeugen Sie eine Ereignis-Hinweistabelle für Produktverkauf
+4. Produkte (die vorhanden sind) dürfen nicht mehr bearbeitet werden
+5. Produkteinkauf bei max. Menge 100 (Bestand) verbieten
+*/
+
+
+---------------------------------------------------------------------------------------
+/*
+1. Verhindern Sie den Verkauf, wenn die Ware nicht auf Lager ist
+*/
+---------------------------------------------------------------------------------------
+
+
+
+---------------------------------------------------------------------------------------
+/*
+2. Nach dem Verkauf Meldung anzeigen, wenn weniger als 5 Artikel vorhanden sind und 
+   einen Bestellhinweis anzeigen, wenn der Bestand geringer als 2 ist
+*/
+---------------------------------------------------------------------------------------
+
+
+---------------------------------------------------------------------------------------
+/*
+3. Erzeugen Sie eine Ereignis-Hinweistabelle für Produktverkauf
+*/
+---------------------------------------------------------------------------------------
+
+
+
+---------------------------------------------------------------------------------------
+/*
+4. Produkte (die vorhanden sind) dürfen nicht mehr bearbeitet werden
+*/
+---------------------------------------------------------------------------------------
+
+
+
+
+---------------------------------------------------------------------------------------
+/*
+5. Produkteinkauf bei max. Menge 100 (Bestand) verbieten
+*/
+---------------------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------------------
+
+
+
+
+
