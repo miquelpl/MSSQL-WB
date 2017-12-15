9c9becdd139248648c19dfbbff71626648e2412d
Miguel Parrilla <miquelpl@telefonica.net>
2017-12-15 14:44:14 +0100

    Add files via upload

---
+++ LOESUNGEN-15122017.sql
@@ -0,0 +1,13 @@
+/*
+Lesen Sie im Buch
+
+Kapitel 12:
+Seite 239 bis 264	Benutzer, Rollen, Rechte
+
+Stellen Sie einen Plan auf wie Sie Benutzer, Rollen und Rechte sinnvoll einsetzen würden.
+Welche Maßnahmen halten Sie für notwendig.
+
+Testen Sie Ihre Rechtevergabe.
+*/
+
+
